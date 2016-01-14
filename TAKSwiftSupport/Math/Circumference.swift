90c48475c785649b48616706ded03dd0945add95
yamamotosaika <yamamotosaika@team-lab.com>
2016-01-14 15:49:46 +0900

    rename and use its funciton

--- TAKSwiftSupport/Math/Circumference.swift
+++ TAKSwiftSupport/Math/Circumference.swift
@@ -1,5 +1,5 @@
 //
-//  Trigonometric.swift
+//  Circumference.swift
 //  TAKSwiftSupport
 //
 //  Created by yamamotosaika on 2016/01/14.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-public class Trigonometric: NSObject {
+public class Circumference: NSObject {
     
     /**
      度数法表記を弧度法表記に変換
@@ -38,12 +38,11 @@ public class Trigonometric: NSObject {
     }
     
     /**
-     サイズと中心座標を指定して半径と弧度から、円周上の座標を算出
+     中心座標を指定して半径と弧度から、円周上の座標を算出
      
      - parameter radius: 半径
      - parameter angle:  角度
      - parameter center: 基準となる中心点の座標
-     - parameter size:   対象サイズ
      
      - returns: 座標
      */
@@ -53,8 +52,8 @@ public class Trigonometric: NSObject {
         center: CGPoint,
         size: CGSize
         ) -> CGPoint {
-            let x = CGFloat(radius * cos(radian)) + center.x - size.width / 2
-            let y = CGFloat(radius * sin(radian)) + center.y - size.height / 2
+            let x = pointOnCircumference(radius: radius, radian: radian).x + center.x
+            let y = pointOnCircumference(radius: radius, radian: radian).y + center.y
             return CGPointMake(x, y)
     }
 }
