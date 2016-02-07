064764be1d860b41f07e139858d292a1092acfe5
nishimura <nishimurataku@team-lab.com>
2016-02-08 05:24:37 +0900

    add Vector2D

--- TAKSwiftSupport/Math/CGPoint+Vector.swift
+++ TAKSwiftSupport/Math/CGPoint+Vector.swift
@@ -37,24 +37,48 @@ public func + (left: CGPoint, right: CGPoint) -> CGPoint {
     return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
 
+func += (inout left: CGPoint, right: CGPoint) {
+    left = left + right
+}
+
 public func - (left: CGPoint, right: CGPoint) -> CGPoint {
     return CGPoint(x: left.x - right.x, y: left.y - right.y)
 }
 
-public func * (left: CGPoint, right: CGFloat) -> CGPoint {
-    return CGPoint(x: left.x * right, y: left.y * right)
+func -= (inout left: CGPoint, right: CGPoint) {
+    left = left - right
 }
 
 public func * (left: CGPoint, right: CGPoint) -> CGPoint {
     return CGPoint(x: left.x * right.x, y: left.y * right.y)
 }
 
+func *= (inout left: CGPoint, right: CGPoint) {
+    left = left * right
+}
+
+public func * (left: CGPoint, right: CGFloat) -> CGPoint {
+    return CGPoint(x: left.x * right, y: left.y * right)
+}
+
+func *= (inout left: CGPoint, right: CGFloat) {
+    left = left * right
+}
+
 public func / (left: CGPoint, right: CGPoint) -> CGPoint {
     return CGPoint(x: left.x / right.x, y: left.y / right.y)
 }
 
+func /= (inout left: CGPoint, right: CGPoint) {
+    left = left / right
+}
+
 public func / (left: CGPoint, right: CGFloat) -> CGPoint {
     return CGPoint(x: left.x / right, y: left.y / right)
+}
+
+func /= (inout left: CGPoint, right: CGFloat) {
+    left = left / right
 }
 
 public func mix(point1 point1: CGPoint, point2: CGPoint, percentage: CGFloat) -> CGPoint {
