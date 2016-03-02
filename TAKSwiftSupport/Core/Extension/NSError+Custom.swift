f66e3fa1f0cfb669a8933137b0744f8b5bae1cd3
nishimura <nishimurataku@team-lab.com>
2016-03-02 20:34:55 +0900

    カスタムエラーコード整理

--- TAKSwiftSupport/Core/Extension/NSError+Custom.swift
+++ TAKSwiftSupport/Core/Extension/NSError+Custom.swift
@@ -11,6 +11,33 @@ import UIKit
 /// NSError汎用
 public extension NSError {
     
+    public enum CustomErrorType: Int {
+        case Unknown = -1
+        
+        // Network
+        case JsonMappingError = -999
+        
+        func localizedDescription() -> String {
+            switch self {
+            case .Unknown:
+                return "Unknown Error"
+                
+            case .JsonMappingError:
+                return "Unable to convert data to string"
+            }
+        }
+    }
+    
+    /**
+     エラータイプを指定してNSError作成
+     */
+    public convenience init(errorType: CustomErrorType) {
+        self.init(
+            domain: AppInfoUtil().bundleIdentifier(),
+            code: errorType.rawValue,
+            userInfo: [NSLocalizedDescriptionKey: errorType.localizedDescription()])
+    }
+    
     /**
      ステータスコードとエラーメッセージを指定してNSError生成
      
