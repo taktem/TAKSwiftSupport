709af3e5356c3717ec151d05827adad894c43344
nishimura <nishimurataku@team-lab.com>
2016-03-17 10:53:26 +0900

    update color extension

--- TAKSwiftSupport/Core/Extension/UIColor+Custom.swift
+++ TAKSwiftSupport/Core/Extension/UIColor+Custom.swift
@@ -39,29 +39,4 @@ public extension UIColor {
     func alpha(alpha: CGFloat) -> UIColor {
         return colorWithAlphaComponent(alpha)
     }
-    
-    // 16進数カラーコードからUIColorを生成する
-    @available(*, deprecated=8.0, message="use UIColor(hexString:)")
-    class func colorWithHexString(string string: String!, alpha:CGFloat) -> UIColor? {
-        
-        var cString:String = string.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet() as NSCharacterSet).uppercaseString
-        
-        if (cString.hasPrefix("#")) {
-            cString = cString.substringFromIndex(cString.startIndex.advancedBy(1))
-        }
-        
-        if (cString.characters.count != 6) {
-            return UIColor.grayColor()
-        }
-        
-        var rgbValue:UInt32 = 0
-        NSScanner(string: cString).scanHexInt(&rgbValue)
-        
-        return UIColor(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
-    }
 }
